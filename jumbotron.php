0af782c96b5201dbde71a67a335728b602f31018
NoahFlowa <osterhoutn@gmail.com>
2022-07-10 00:20:39 -0400

    Added Jumbotron

---
+++ jumbotron.php
@@ -0,0 +1,6 @@
+<div class="p-5 mb-4 rounded-3">
+    <div class="container-fluid py-5">
+        <h1 class="display-3 fw-bold"><span class="typeWriter" data-end="5000" data-speed="2" data-text='["Hi, my name is Noah!","Welcome to my website!"]'></span></h1>
+        <p class="col-md-8 fs-2">I am an Applications Developer for Web, Mobile and Desktop focused on creating great codebases and even better user interfaces.</p>
+    </div>
+</div>
