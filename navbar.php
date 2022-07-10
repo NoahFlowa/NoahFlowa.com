1ae9fb0c639b0773f529cf1f5cad440a3af00141
NoahFlowa <osterhoutn@gmail.com>
2022-07-10 00:20:54 -0400

    Added navbar

---
+++ navbar.php
@@ -0,0 +1,27 @@
+<nav class="navbar navbar-expand-lg navbar-dark sticky-top fs-4 pt-3" style="background-color: #101317;">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="https://noahosterhout.com"><span style="font-family: abberancy, 'Open Sans';">Noah <span style="color: #22c2fc; font-family: abberancy;">O</span>sterhout </span></a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                <li class="nav-item">
+                    <a class="nav-link" style="color: #22c2fc;" href="https://github.com/NoahFlowa"> github</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" style="color: #22c2fc;" href="https://gitlab.com/NoahFlowa">gitlab</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" style="color: #22c2fc;" href="https://stackoverflow.com/users/9227487/noah-osterhout?tab=profile">stack overflow</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" style="color: #22c2fc;" href="https://stackexchange.com/users/12700630/noah-osterhout">stack exchange</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" style="color: #22c2fc;" href="https://www.linkedin.com/in/noah-osterhout-732171149">linkedin</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
