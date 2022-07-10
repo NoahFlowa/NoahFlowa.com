14875073b30f9f399a99c89615940af16932738f
NoahFlowa <osterhoutn@gmail.com>
2022-07-10 01:14:48 -0400

    Working commit | Adding new timeline

---
+++ experience.php
@@ -0,0 +1,58 @@
+<div class="container-fluid mt-5">
+    <ul class="timeline-with-icons">
+        <li class="timeline-item mb-5">
+            <span class="timeline-icon">
+                <i class="fas fa-rocket text-primary fa-sm fa-fw"></i>
+            </span>
+
+            <h5 class="fw-bold">What I am currently up to...</h5>
+            <p class=" mb-2 fw-bold">Present Day</p>
+            <p class="">
+                Full-Time Student at Ferris State University for a Bachelors of Science in Computer Information Systems and enjoying my last summer break of no work :D
+            </p>
+        </li>
+
+        <li class="timeline-item mb-5">
+
+            <span class="timeline-icon">
+                <i class="fas fa-hand-holding-usd text-primary fa-sm fa-fw"></i>
+            </span>
+            <h5 class="fw-bold">What did I do during the second year of the pandemic?</h5>
+            <p class=" mb-2 fw-bold">2021</p>
+            <p class="">
+                OK Enterprises changes to Osterhout & Klein Enterprises LLC. however the partners decide to close it as we all pursue other ventures and fields than what the company was made for. I personally learned a lot about owning and operating a business as well as how to set your own goals and see them finalized.
+            </p>
+        </li>
+
+        <li class="timeline-item mb-5">
+
+            <span class="timeline-icon">
+                <i class="fas fa-users text-primary fa-sm fa-fw"></i>
+            </span>
+            <h5 class="fw-bold">Our team exceeds 10 people</h5>
+            <p class=" mb-2 fw-bold">24 June 2020</p>
+            <p class="">
+                Orci varius natoque penatibus et magnis dis parturient montes,
+                nascetur ridiculus mus. Nulla ullamcorper arcu lacus, maximus
+                facilisis erat pellentesque nec. Duis et dui maximus dui aliquam
+                convallis. Quisque consectetur purus erat, et ullamcorper sapien
+                tincidunt vitae.
+            </p>
+        </li>
+
+        <li class="timeline-item mb-5">
+
+            <span class="timeline-icon">
+                <i class="fas fa-money-bill-wave text-primary fa-sm fa-fw"></i>
+            </span>
+            <h5 class="fw-bold">Earned the first million $!</h5>
+            <p class=" mb-2 fw-bold">15 October 2020</p>
+            <p class="">
+                Nulla ac tellus convallis, pulvinar nulla ac, fermentum diam. Sed
+                et urna sit amet massa dapibus tristique non finibus ligula. Nam
+                pharetra libero nibh, id feugiat tortor rhoncus vitae. Ut suscipit
+                vulputate mattis.
+            </p>
+        </li>
+    </ul>
+</div>
