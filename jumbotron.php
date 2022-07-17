5e364eb753dea34a82ac0e9449c5b9e4d6e420a9
NoahFlowa <osterhoutn@gmail.com>
2022-07-17 01:57:19 -0400

    Updated jumbotron

--- jumbotron.php
+++ jumbotron.php
@@ -1,12 +1,6 @@
 <div class="mb-4 mt-4 rounded-3">
     <div class="container-fluid py-5">
-        <h1 class="display-3 fw-bold"><span class="typeWriter" data-end="5000" data-speed="2" data-text='["Hi, my name is Noah!","Welcome to my website!"]'></span></h1>
-        <p class="col-md-8 fs-2">I am an Applications Developer for Web, Mobile and Desktop focused on creating great codebases and even better user interfaces.</p>
-        <a class="btn btn-outline-primary" data-bs-toggle="collapse" href="#collapseMoreInfo" role="button" aria-expanded="false" aria-controls="collapseMoreInfo">Want to know more?</a>
-        <div class="collapse" id="collapseMoreInfo">
-            <div class="card card-body border-0 mt-2" style="background-color: #101317;">
-                <p class="fs-3">I am currently pursuing my last semester at Ferris State University for my Bachelors of Science in Computer Information Systems. I also love to hang out with my friends in beautiful Traverse City, Michigan and play on my custom built gaming PC. I also love to dabble in Game Creation with Unreal Engine 5 and even Virtual and Augmented Reality with my Meta Quest 2 and the HTC Vive.</p>
-            </div>
-        </div>
+        <h1 class="display-2 fw-bold"><span class="typeWriter" data-end="5000" data-speed="2" data-text='["Hi, my name is Noah!","Welcome to my website!"]'></span></h1>
+        <p class="col-md-8 fs-3">I am an Applications Developer for Web, Mobile and Desktop focused on creating great codebases and even better user interfaces.</p>
     </div>
 </div>
