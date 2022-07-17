ea4ce86b76cd250c47cbf7a805636887e51d6f0b
NoahFlowa <osterhoutn@gmail.com>
2022-07-17 02:41:48 -0400

    Changed margin bottom and top to margin y class for Bootstrap guidelines

--- jumbotron.php
+++ jumbotron.php
@@ -1,4 +1,4 @@
-<div class="mb-4 mt-4 rounded-3">
+<div class="my-5 rounded-3">
     <div class="container-fluid py-5">
         <h1 class="display-2 fw-bold"><span class="typeWriter" data-end="5000" data-speed="2" data-text='["Hi, my name is Noah!","Welcome to my website!"]'></span></h1>
         <p class="col-md-8 fs-3">I am an Applications Developer for Web, Mobile and Desktop focused on creating great codebases and even better user interfaces.</p>
