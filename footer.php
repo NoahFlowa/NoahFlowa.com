e83f26544e538f54776da2c7e2672a0ddd5150cd
NoahFlowa <osterhoutn@gmail.com>
2022-07-17 01:57:33 -0400

    Added footer

---
+++ footer.php
@@ -0,0 +1,29 @@
+<div class="container-fluid pt-5">
+    <footer class="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
+        <p class="col-md-4 mb-0 fs-5">&copy; <span style="font-family: abberancy, 'Open Sans';">Noah <span style="color: #22c2fc; font-family: abberancy;">O</span>sterhout </span> - <?php echo date("Y") ?></p>
+
+        <a href="/" class="col-md-4 d-flex align-items-center justify-content-center mb-3 mb-md-0 me-md-auto link-dark text-decoration-none">
+            <svg class="bi me-2" width="40" height="32">
+                <use xlink:href="#bootstrap" />
+            </svg>
+        </a>
+
+        <ul class="nav col-md-4 justify-content-end">
+            <li class="nav-item">
+                <a class="nav-link" style="color: #22c2fc;" href="https://github.com/NoahFlowa"> github</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" style="color: #22c2fc;" href="https://gitlab.com/NoahFlowa">gitlab</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" style="color: #22c2fc;" href="https://stackoverflow.com/users/9227487/noah-osterhout?tab=profile">stack overflow</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" style="color: #22c2fc;" href="https://stackexchange.com/users/12700630/noah-osterhout">stack exchange</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" style="color: #22c2fc;" href="https://www.linkedin.com/in/noah-osterhout-732171149">linkedin</a>
+            </li>
+        </ul>
+    </footer>
+</div>
