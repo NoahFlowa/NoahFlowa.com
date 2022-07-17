f7555656b881e36dc3b7fb8c9925549b336ce1db
NoahFlowa <osterhoutn@gmail.com>
2022-07-17 01:58:11 -0400

    Added skills

---
+++ skills.php
@@ -0,0 +1,58 @@
+<div class="container-fluid">
+    <h1 class="display-5 py-5 text-center fw-bold">A Brief Look At Some Of My Projects</h1>
+    <div class="row">
+        <div class="col-sm-3">
+            <div class="card">
+                <div class="card-body">
+                    <p class="text-center pb-4"><img src="/img/NO_Logo.svg" width="150" height="150"></p>
+                    <h5 class="card-title fw-bold">Noah Osterhout Website</h5>
+                    <p class="card-text fs-5">This websites source code is available on my GitHub!</p>
+                    <a href="https://github.com/NoahFlowa/NoahOsterhout.com" class="btn btn-outline-primary">View GitHub Repo</a>
+                </div>
+            </div>
+        </div>
+        <div class="col-sm-4">
+            <div class="card">
+                <div class="card-body">
+                    <p class="text-center pb-4"><img src="/img/chatversity-logo.svg" width="150" height="150"></p>
+                    <h5 class="card-title fw-bold">Chatversity</h5>
+                    <p class="card-text fs-5">
+                        Winning Capstone project of spring 2019 where the team had the idea to make it simpler for college students to create
+                        school groups and find them.
+                    </p>
+                    <a href="https://github.com/NoahsNMC/Chatversity_App" class="btn btn-outline-primary">View GitHub Repo</a>
+                </div>
+            </div>
+        </div>
+        <div class="col-sm-5">
+            <div class="card">
+                <div class="card-body">
+                    <p class="text-center pb-4"><img src="https://fakerphp.github.io/images/logo.svg" width="130" height="130"></p>
+                    <h5 class="card-title fw-bold">WordPress Lorem Ipsum Generator with Faker via Composer</h5>
+                    <p class="card-text fs-5">
+                        Fork of https://github.com/MatteoManna/WP-Lorem-ipsum but I implemented Faker generator via Composer which actually will generate
+                        semi real data instead of regular Lorem Ipsum data. This can be used to fill out various fields as placeholders or any other
+                        reason you would want to use Lorem Ipsum.
+                    </p>
+                    <a href="https://github.com/NoahFlowa/WP_Lorem_Generator" class="btn btn-outline-primary">View GitHub Repo</a>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-sm-5">
+            <div class="card">
+                <div class="card-body">
+                    <p class="text-center pb-4"><i class="fa-solid fa-boxes-stacked fa-6x"></i></p>
+                    <h5 class="card-title fw-bold">ISYS 431 Senior Capstone ERP</h5>
+                    <p class="card-text fs-5">
+                        I cannot show this repo as I signed an NDA, however, for my final capstone class we won the competition
+                        as we built an ERP program for a client that allowed them to manage their products from design to shipping
+                        out to customers. This project was built using Angular, NodeJS, TypeScript, MSSQL and .NET.
+                    </p>
+                    <a href="#" class="btn btn-outline-primary disabled">View GitHub Repo</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
