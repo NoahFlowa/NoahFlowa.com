5b5282bcafbc7784a8c94ab87c7ea903e05e4d2e
NoahFlowa <osterhoutn@gmail.com>
2023-05-05 20:33:34 -0400

    Added The Boyos Discord Bot section

--- skills.php
+++ skills.php
@@ -1,12 +1,22 @@
 <div class="container-fluid">
     <h1 class="display-5 py-5 text-center fw-bold">A Brief Look At Some Of My Projects</h1>
     <div class="row">
-        <div class="col-sm-3">
+        <div class="col-sm-4">
             <div class="card">
                 <div class="card-body">
                     <p class="text-center pb-4"><img src="/img/NO_Logo_Text.svg" width="150" height="150"></p>
                     <h5 class="card-title fw-bold">Noah Osterhout Website</h5>
                     <p class="card-text fs-5">This websites source code is available on my GitHub!</p>
+                    <a href="https://github.com/NoahFlowa/NoahOsterhout.com" class="btn btn-outline-primary">View GitHub Repo</a>
+                </div>
+            </div>
+        </div>
+        <div class="col-sm-4">
+            <div class="card">
+                <div class="card-body">
+                    <p class="text-center pb-4"><img src="/img/BoyosDiscordLogo.png" width="200" height="200"></p>
+                    <h5 class="card-title fw-bold">The Boyos Discord Bot</h5>
+                    <p class="card-text fs-5">The Boyos Discord Bot is a custom NodeJS and JavaScript Discord moderation, music playing and misc bot created by me and one of my close friend <a href="https://github.com/wymillerlinux" target="_blank">Wyatt J. Miller</a>!</p>
                     <a href="https://github.com/NoahFlowa/NoahOsterhout.com" class="btn btn-outline-primary">View GitHub Repo</a>
                 </div>
             </div>
@@ -24,23 +34,9 @@
                 </div>
             </div>
         </div>
-        <div class="col-sm-5">
-            <div class="card">
-                <div class="card-body">
-                    <p class="text-center pb-4"><img src="https://fakerphp.github.io/images/logo.svg" width="130" height="130"></p>
-                    <h5 class="card-title fw-bold">WordPress Lorem Ipsum Generator with Faker via Composer</h5>
-                    <p class="card-text fs-5">
-                        Fork of <a href="https://github.com/MatteoManna/WP-Lorem-ipsum" target="_blank">MatteoManna WP Lorem Ipsum</a> but I implemented Faker generator via Composer which actually will generate
-                        semi real data instead of regular Lorem Ipsum data. This can be used to fill out various fields as placeholders or any other
-                        reason you would want to use Lorem Ipsum.
-                    </p>
-                    <a href="https://github.com/NoahFlowa/WP_Lorem_Generator" class="btn btn-outline-primary">View GitHub Repo</a>
-                </div>
-            </div>
-        </div>
     </div>
     <div class="row">
-        <div class="col-sm-5">
+        <div class="col-sm-6">
             <div class="card">
                 <div class="card-body">
                     <p class="text-center pb-4"><i class="fa-solid fa-boxes-stacked fa-6x"></i></p>
@@ -51,6 +47,20 @@
                         the client wanted for his business.
                     </p>
                     <a href="#" class="btn btn-outline-primary disabled">View GitHub Repo</a>
+                </div>
+            </div>
+        </div>
+        <div class="col-sm-6">
+            <div class="card">
+                <div class="card-body">
+                    <p class="text-center pb-4"><img src="https://fakerphp.github.io/images/logo.svg" width="130" height="130"></p>
+                    <h5 class="card-title fw-bold">WordPress Lorem Ipsum Generator with Faker via Composer</h5>
+                    <p class="card-text fs-5">
+                        Fork of <a href="https://github.com/MatteoManna/WP-Lorem-ipsum" target="_blank">MatteoManna WP Lorem Ipsum</a> but I implemented Faker generator via Composer which actually will generate
+                        semi real data instead of regular Lorem Ipsum data. This can be used to fill out various fields as placeholders or any other
+                        reason you would want to use Lorem Ipsum.
+                    </p>
+                    <a href="https://github.com/NoahFlowa/WP_Lorem_Generator" class="btn btn-outline-primary">View GitHub Repo</a>
                 </div>
             </div>
         </div>
